cd77e0345cba077138de2db3dd7a39948df2d7f2
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2024-07-12 14:04:50 +0200

    Add files via upload

---
+++ if.cpp
@@ -0,0 +1,13 @@
+// if statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int number = 18;
+   if(number > 0) {
+      cout << "number is positive" << endl
+   }
+
+   return 0;
+}
