dcc2579acdda40f2ae4f0b1e18612b557d740951
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2024-07-12 14:03:51 +0200

    Add files via upload

---
+++ math002.cpp
@@ -0,0 +1,15 @@
+// using the cmath library
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+   double x = 16.0;
+   
+   cout << "Square root: " << sqrt(x) << endl;
+   cout << "Power: " << pow(x, 2) << endl;
+   cout << "Logarithm: " << log(x) << endl;
+
+   return 0;
+}
