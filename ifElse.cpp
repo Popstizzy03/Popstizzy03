018678067cb16e7a046e1214296405e17d3f1a28
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2024-07-12 14:05:13 +0200

    Add files via upload

---
+++ ifElse.cpp
@@ -0,0 +1,15 @@
+// if-else statement
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int number = -5;
+   if (number > 0) {
+	   cout << "Number is positive" << endl;
+} else {
+   cout << "Number is negative" << endl;
+}
+
+return 0;
+}
