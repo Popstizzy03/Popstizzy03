4c84ab904c2ff4ef3fcdef17014a4b58e60fff8c
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2024-07-12 14:06:41 +0200

    Add files via upload

---
+++ ternary.cpp
@@ -0,0 +1,12 @@
+// Ternary operator
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a = 10, int b = 20;
+   int max = (a > b) ? a : b;
+   cout << "Max Value: " << max << endl;
+
+   return 0;
+}
