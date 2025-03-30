d41850f28daec8c5c800cc64ab0e7b111ecc21ce
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-03-31 00:09:20 +0200

    Update if.cpp
    removed "using namespace std" to prevent namespace pollution

--- if.cpp
+++ if.cpp
@@ -1,13 +1,14 @@
-// if statement
-
 #include <iostream>
-using namespace std;
 
 int main() {
-   int number = 18;
-   if(number > 0) {
-      cout << "number is positive" << endl
-   }
+    int number = 18;
+    if (number > 0) {
+        if (std::cout) {
+            std::cout << "number is positive" << std::endl;
+        } else {
+            std::cerr << "Error: Output stream is in a bad state." << std::endl;
+        }
+    }
 
-   return 0;
+    return 0;
 }
