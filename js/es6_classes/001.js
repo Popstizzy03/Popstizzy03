a83c60add18a89332371516364616767e6813f8a
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-17 13:29:57 +0200

    Create 001.js

---
+++ js/es6_classes/001.js
@@ -0,0 +1,11 @@
+class Rectangle {
+  constructor(height, width) {
+    this.height = height
+		this.width = width
+  }
+}
+
+const square = new Rectangle (5, 5);
+const reactangle = new Reactangle(4, 5);
+
+console.log(square.height);
