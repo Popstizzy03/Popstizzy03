7950767d39a3c37bb7f929a85e4131895abafbf8
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-17 13:37:01 +0200

    Create 002.js

---
+++ js/es6_classes/002.js
@@ -0,0 +1,19 @@
+class Rectangle {
+  constructor (height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  get area() {
+    return this.calcArea;
+  } 
+
+  calcArea() {
+    return this.width * this.height;
+  }
+}
+
+const square = new Rectangle(5, 5)
+
+console.log(square.area);
+
