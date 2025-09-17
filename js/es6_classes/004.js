9c46dccbe12f077621919035c2890d8d02492292
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-17 13:44:12 +0200

    Create 004.js

---
+++ js/es6_classes/004.js
@@ -0,0 +1,16 @@
+class
+ Human {
+  constructor(name) {
+    this.name = name;
+  }
+}
+class Student 
+extends
+ Human {
+  constructor(name, age) {    
+    
+super
+(name);
+    this.age = age;
+  }
+}
