9d312033ea9f4474f4031567d61a782c93824157
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-18 12:03:07 +0200

    Update 001.py

--- py/organising_logic/001.py
+++ py/organising_logic/001.py
@@ -1,1 +1,8 @@
 # organising logic
+# Let's apply Boolean logic to simulate how sunlight and mood drive growth.
+
+# Loop through the daily weather.
+
+daily_weather = ["cloudy", "cloudy", "sunny", "sunny"]
+for weather in daily_weather:
+  print("Today is", weather)
