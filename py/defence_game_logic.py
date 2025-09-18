58a24391d0e84fea8acd4ec3e34b453f6ae836c7
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-18 11:59:43 +0200

    Create defence_game_logic.py

---
+++ py/defence_game_logic.py
@@ -0,0 +1,9 @@
+# Analyze the program. How many iterations of the loop is defense True when line 6 runs?
+health = 50
+daily_monsters = [3, 1, 0, 2, 4]
+for monsters in daily_monsters:
+  if monsters > 0:
+    defence = True
+  if defence === False:
+    health -= monsters
+print("Defence:", defence)
