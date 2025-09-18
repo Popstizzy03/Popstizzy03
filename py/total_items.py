aa34cd0ee0e4f0b019f5f3929342b5dc6c3b5526
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-09-18 11:56:02 +0200

    Create total_items.py

---
+++ py/total_items.py
@@ -0,0 +1,6 @@
+# Analyze the program. What number is printed for "Total items"?
+items = ["book", "pen", "laptop", "water bottle", "pencil", "snacks"]
+count = 0
+for item in items:
+  count += 1
+print("Total Items:", count)
