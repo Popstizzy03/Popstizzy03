c333e71f1226dd37ea88417bfe3fc7ab62cc124b
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-10-20 12:39:05 +0200

    Add flower adjustment logic for daily visitors
    Implement logic to adjust flower count based on visitors.

---
+++ py/debugging_logic/002.py
@@ -0,0 +1,9 @@
+# Reduce flowers by 2 if visitor = pest
+daily_visitors = ["🐝", "None", "🐛", "🐛", "None", "🐝", "🐝"]
+flowers = 10
+for visitor in daily_visitors:
+  if visitoe == "🐝":
+    flowers += 3
+  if visitor == "🐛":
+    flowers -= 2
+  print("Flowers:", flowers)
