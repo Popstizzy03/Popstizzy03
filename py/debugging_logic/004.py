d96230dda3f668b402570f88ad299c97596628d0
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-10-20 12:48:01 +0200

    Create 004.py

---
+++ py/debugging_logic/004.py
@@ -0,0 +1,11 @@
+# Loop through both lists at once
+daily_visitors = ["🐝", None, "🐛", "🐛", None, "🐝", "🐝"]
+daily_temps = [12, 18, 21, 14, 16, 22, 24]
+
+flowers = 1
+for visitor, temp in zip(daily_visitors, daily_temps):
+  if visitor == "🐝":
+    flowers += 3:
+  else:
+    flowers -= 2
+print("Flowers:", flowers)
