93389687cc75abc4eb24dce51a14c3d44baa4ad2
Rabboni Kabongo <138282978+Popstizzy03@users.noreply.github.com>
2025-10-24 12:21:46 +0200

    Add if checks for rain and worms

---
+++ py/if_check.py
@@ -0,0 +1,8 @@
+daily_worms = [True, False, True, True, False]
+daily_rain = [True, False, True, False, True]
+for worms, rain in zip(daily_worms, daily_rain):
+  if rain:
+    if worms:
+      print("rain and worms")
+    else:
+      print("rain but no worms")
